6b69564b6fffcdbd680128436b7a03f7fe806b12
Lucky Pathan <lckpathan@gmail.com>
2025-12-15 22:56:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const { generateRandomNumber, celicusToFahrenheit } = require("./utils");
+
+console.log(
+  `Random Number: ${generateRandomNumber()} and Fahrenheit: ${celicusToFahrenheit(
+    10
+  )}`
+);
